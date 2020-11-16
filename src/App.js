7dbc572aae5b915ee787958a6a93262133c98fa5
FuChai Mead <fuchaimead@FuChais-MacBook-Pro.local>
2020-11-16 12:59:13 -0700

    add timer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { fetchTechnicians } from './actions';
 import Map from './Map';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [minute, setMinute] = useState(0);
+  const timer = () => setMinute(minute + 1);
+
   useEffect(() => {
     fetchTechnicians(data => setData(data));
   }, []);
+
+  useEffect(() => {
+      if (minute === 15) { return; }
+      const id = setInterval(timer, 1000);
+      return () => clearInterval(id);
+    },[minute]);
+
+  if(data === null ) { return(<div>Loading data....</div>) }
 
   return (
     <div className="App">
       <header>
         <h1>Solar Technicians</h1>
       </header>
-      <Map data={data}/>
+      <Map data={data} minute={minute}/>
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -5,37 +5,36 @@ import './Map.css';
 mapboxgl.accessToken = 'pk.eyJ1IjoiamZtZWFkMTYiLCJhIjoiY2toa3ExdG93MHdxcjJycDU2b2pnNjY1NCJ9.s_OM0pZ7AnnkN65MaZjpkA';
 
 const Map = (props) => {
+  const siteId = 200;
   const mapContainerRef = useRef(null);
-  const [lng, setLng] = useState(40.5);
-  const [lat, setLat] = useState(60);
-  const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat], // starting position [lng, lat]
-      zoom: zoom // starting zoom
+      center: props.data[siteId][props.minute].features[0].geometry.coordinates, // starting position [lng, lat]
+      zoom: 12 // starting zoom
     });
 
     // Add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
+    props.data[siteId][props.minute].features.map(item => {
+      new mapboxgl.Marker()
+      .setLngLat(item.geometry.coordinates)
+      .setRotation(item.properties.bearing)
+      .setPopup(new mapboxgl.Popup().setHTML(item.properties.name)) // todo show name of technician without popup
+      .addTo(map);  
     });
 
-    const marker = new mapboxgl.Marker()
-    .setLngLat([30.5, 50.5])
-    .addTo(map);
-
     return () => map.remove();
-  }, []);
+  }, [props.minute, props.data]);
 
+  // handle case of no technicians found
+  const timeUpdated = new Date(props.data[siteId][props.minute].features[0].properties.tsecs * 1000).toString();
   return (
     <div>
+      <p>Updated: <strong>{timeUpdated}</strong></p>
       <div className='map-container' ref={mapContainerRef} />
     </div>
   )
